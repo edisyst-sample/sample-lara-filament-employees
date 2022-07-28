fc8d076fc4e173e80022b588a81842c06fb4d40d
e.testagrossa <e.testagrossa@digitalpa.it>
2022-07-28 18:14:17 +0200

    Importare le classi

---
+++ README.md
@@ -0,0 +1,15 @@
+
+```php
+php artisan breeze:install
+npm install
+npm run dev
+php artisan migrate
+composer require filament/filament:"^2.0"
+php artisan make:filament-user
+```
+
+In App\Http\Requests\Auth\LoginRequest imposto 
+- num_max di tentativi di login 
+- tempo di attesa = 5 minuti = 300 seconds
+
+php artisan make:filament-resource Country
---
+++ app/Filament/Resources/EmployeeResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EmployeeResource\Pages;
+use App\Filament\Resources\EmployeeResource\RelationManagers;
+use App\Models\Employee;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Filters\SelectFilter;
+
+use App\Models\Country;
+use App\Models\State;
+use App\Models\City;
+
+class EmployeeResource extends Resource
+{
+    protected static ?string $model = Employee::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        Select::make('country_id')
+                            ->label('Country')
+                            ->options(Country::all()->pluck('name', 'id')->toArray())
+                            ->reactive()
+                            ->afterStateUpdated(fn (callable $set) => $set('state_id','null'))
+                            ,
+                        Select::make('state_id')
+                            ->label('State')
+                            ->options(function(callable $get){
+                                $country = Country::find($get('country_id'));
+                                return ($country)
+                                    ? $country->states->pluck('name', 'id')
+                                    : State::all()->pluck('name', 'id');
+                                })
+                            ->reactive()
+                            ->afterStateUpdated(fn (callable $set) => $set('city_id','null'))
+                            ,
+                        Select::make('city_id')
+                            ->label('City')
+                            ->options(function(callable $get){
+                                $state = State::find($get('state_id'));
+                                return ($state)
+                                    ? $state->cities->pluck('name', 'id')
+                                    : City::all()->pluck('name', 'id');
+                                }),
+                        Select::make('department_id')->relationship('department', 'name'),
+                        TextInput::make('first_name')->required(),
+                        TextInput::make('last_name')->required(),
+                        TextInput::make('address')->required(),
+                        TextInput::make('zip_code')->required(),
+
+                        DatePicker::make('birth_date')->required(),
+                        DatePicker::make('date_hired')->required(),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('department.name')->sortable(),
+                TextColumn::make('first_name')->sortable()->searchable(),
+                TextColumn::make('last_name')->sortable()->searchable(),
+                TextColumn::make('date_hired')->date(),
+            ])
+            ->filters([
+                SelectFilter::make('department')->relationship('department', 'name')
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEmployees::route('/'),
+            'create' => Pages\CreateEmployee::route('/create'),
+            'edit' => Pages\EditEmployee::route('/{record}/edit'),
+        ];
+    }
+}
